274f52d3ab15e10da382e4ccaa719d30a5c4298f
JacobZwang <zwang.jacob@gmail.com>
2020-08-08 00:58:11 -0700

    add profile photo and sass

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="sideBarWrapper"></div>
+  <div class="sideBarWrapper">
+    <img
+      src="https://media-exp1.licdn.com/dms/image/C5603AQGP96mu1J-t8g/profile-displayphoto-shrink_400_400/0?e=1602115200&amp;v=beta&amp;t=YZ-aubC-7x9QwDXA26e1-PO9x08yUgYSXhquYfCi1kE"
+    />
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .sideBarWrapper {
   background-color: lightgrey;
   height: 100%;
@@ -11,5 +15,15 @@
 }
 :hover.sideBarWrapper {
   width: 200pt;
+  img {
+    border-radius: 10px;
+    transition: 300ms ease-out;
+  }
+}
+
+img {
+  width: 100%;
+  border-radius: 50%;
+  transition: 300ms ease-out;
 }
 </style>
