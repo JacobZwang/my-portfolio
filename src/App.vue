28960bb8d6dc9b3ca160773a69997e6f99f8ef54
JacobZwang <zwang.jacob@gmail.com>
2020-08-08 00:26:45 -0700

    create sidebar animation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="appWrapper">
+      <Portfolio />
+      <SideBar />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Portfolio from "./components/Portfolio.vue";
+import SideBar from "./components/SideBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Portfolio,
+    SideBar,
+  },
+};
 </script>
 
 <style>
@@ -23,6 +27,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  margin: 0px;
+}
+
+.appWrapper {
+  display: flex;
+  height: 100vh;
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sideBarWrapper"></div>
+</template>
+
+<style scoped>
+.sideBarWrapper {
+  background-color: lightgrey;
+  height: 100%;
+  width: 40pt;
+  transition: 300ms;
+}
+:hover.sideBarWrapper {
+  width: 200pt;
+}
+</style>
