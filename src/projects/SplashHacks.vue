73bd5d9f7202ea5a12f979d496c919d21fef33bd
JacobZwang <zwang.jacob@gmail.com>
2020-08-08 18:00:31 -0700

    push to computer

---
+++ src/projects/SplashHacks.vue
@@ -0,0 +1,11 @@
+<template>
+  <Project></Project>
+</template>
+<script>
+import Project from "@/templates/Project.vue";
+export default {
+  components: {
+    Project,
+  },
+};
+</script>
---
+++ src/templates/Project.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="portfolioWrapper">
+    <div class="infoColumn">
+      <div style="margin: 10pt;">
+        <slot name="info"></slot>
+      </div>
+    </div>
+    <div class="displayColumn">
+      <slot name="display"></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.portfolioWrapper {
+  display: flex;
+  grid-template-columns: auto;
+  grid-template-rows: auto;
+  width: 100%;
+  height: 100vh;
+}
+.infoColumn {
+  width: 50%;
+  height: 100%;
+  overflow-y: scroll;
+  transition: 400ms;
+}
+.displayColumn {
+  width: 50%;
+  height: 100%;
+  transition: 400ms;
+}
+:hover.infoColumn {
+  width: 80%;
+  transition: 400ms;
+  .displayColumn {
+    width: 20%;
+    transition: 400ms;
+  }
+}
+:hover.displayColumn {
+  width: 80%;
+  transition: 400ms;
+  .infoColumn {
+    width: 20%;
+    transition: 400ms;
+  }
+}
+</style>
