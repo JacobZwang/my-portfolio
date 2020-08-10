bdcf05a6bd072a8e09a54a2e4f7a0cd83131eaa4
jacobzwang <zwang.jacob@gmail.com>
2020-08-10 12:29:06 -0700

    small fixes

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -5,25 +5,27 @@
         <h1>Splash Hacks</h1>
         <h3>Splash Hacks is an online Hackathon founded by Ben Swerdlow</h3>
         <h2>Brand Design</h2>
-        <p1
-          >I named the Hackathon Splash Hacks to emphasize a couple different
+        <p1>
+          I named the Hackathon Splash Hacks to emphasize a couple different
           things. The Founder, Ben Swerdlow, wanted to suggest the place of
           origin, Los Angeles, without alienating outsiders. I chose the word
           splash specifically because of it's dual meaning. A splash suggests
-          that there is a big impact, with widespread reach. </p1
-        ><p1
-          >I also created the logo. The word splash is a completely custom
+          that there is a big impact, with widespread reach.
+        </p1>
+        <p1>
+          I also created the logo. The word splash is a completely custom
           typeface created just for Splash Hacks and the word hack was a
           carefully chosen font to help maintain brand usability across many
-          platforms. </p1
-        ><p1>
+          platforms.
+        </p1>
+        <p1>
           Lastly I created the color pallet, style guide, and use cases along
           with Jessica Golden.
         </p1>
         <h2>Web Development</h2>
         <p1>
           I led the web development team in creating the Splash Hacks website in
-          Vue JS. My team was infemailar with Vue, however I believed it was the
+          Vue JS. My team was unfamiliar with Vue, however I believed it was the
           best option so quickly got my team up to speed and had them developing
           in no time. To further speed up production, we used agile development,
           meaning we pushed small improvements frequently rather than waiting
